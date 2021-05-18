9500ab70992ac45c4087436d61207c0207c6a97b
Chuyukang <17082643+Chuyukang@users.noreply.github.com>
2021-05-18 20:43:58 +0800

    fix test error

--- pkg/k8s/load_balancer_test.go
+++ pkg/k8s/load_balancer_test.go
@@ -3,9 +3,7 @@ package k8s
 import (
 	"fmt"
 	"k8s.io/apimachinery/pkg/api/resource"
-	"strconv"
 	"testing"
-	"time"
 )
 
 func TestRoundRobinLB_GetBackend(t *testing.T) {
@@ -27,15 +25,15 @@ func TestRoundRobinLB_GetBackend(t *testing.T) {
 func TestWeightedRRLB_GetBackend(t *testing.T) {
 	upstreams := []string{"10.0.0.1", "10.0.0.2", "10.0.0.3"}
 	fetcher := NewFakeUpstreamFetcher(upstreams)
-	lb := &WeightedRRLB{endpointWeight: []int{1,1,3}, fetcher: fetcher}
+	lb := &WeightedRRLB{endpointWeight: []int{1, 1, 3}, fetcher: fetcher}
 
 	bucket := make(map[string]int)
 	for i := 0; i < 1000; i++ {
 		backend, err := lb.GetBackend()
-		if err!=nil {
+		if err != nil {
 			t.Fail()
 		}
-		bucket[backend]+=1
+		bucket[backend] += 1
 	}
 
 	for k, v := range bucket {
@@ -47,40 +45,30 @@ func TestLeastCPULB_GetBackend(t *testing.T) {
 	upstreams := []string{"10.0.0.1", "10.0.0.2", "10.0.0.3"}
 	fetcher := NewFakeUpstreamFetcher(upstreams)
 
-	lb := &LeastCPULB{fetcher: fetcher, functionName: "",namespace: "",
-		index: PodMetricsIndex{
-		index: map[string]*PodSimpleMetrics{
-		"10.0.0.1":{resource.NewScaledQuantity(0,0),nil},
-		"10.0.0.2":{resource.NewScaledQuantity(1,0),nil},
-		"10.0.0.3":{resource.NewScaledQuantity(2,0),nil},
+	lb := &LeastCPULB{fetcher: fetcher, functionName: "", namespace: "",
+		index: PodMetricsIndex{index: map[string]*PodSimpleMetrics{
+			"10.0.0.1": {resource.NewScaledQuantity(1, 0), nil},
+			"10.0.0.2": {resource.NewScaledQuantity(2, 0), nil},
+			"10.0.0.3": {resource.NewScaledQuantity(3, 0), nil}},
 		},
-		},
-}
-	go func(index *PodMetricsIndex) {
-		lastSelect := 0
-		for ; ; {
-			index.mu.Lock()
-			for i:=0;i<3;i++ {
-				index.index["10.0.0."+strconv.Itoa(i+1)].PodCPU = resource.NewScaledQuantity(10,0)
-			}
+	}
+	backend1, err := lb.GetBackend()
+	if err != nil {
+		t.Fail()
+	}
 
-			aim := "10.0.0."+strconv.Itoa(lastSelect+1)
-			index.index[aim].PodCPU=resource.NewScaledQuantity(0,0)
-			index.mu.Unlock()
-			lastSelect = (lastSelect+1)%3
+	if backend1 != "10.0.0.1" {
+		t.Fail()
+	}
 
-			time.Sleep(10 * time.Second)
-		}
-	}(&lb.index)
+	lb.index.index["10.0.0.2"].PodCPU=resource.NewScaledQuantity(0,0)
+	backend2, err := lb.GetBackend()
+	if err != nil {
+		t.Fail()
+	}
 
-	for ;; {
-		backend,err := lb.GetBackend()
-		if err!=nil {
-			t.Fail()
-		}
-
-		fmt.Println(backend)
-		time.Sleep(time.Second*10)
+	if backend2 != "10.0.0.2" {
+		t.Fail()
 	}
 }
 
